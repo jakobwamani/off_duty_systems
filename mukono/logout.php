3e2c4709047490ddfe0576f5754edce1c7914116
wamani jacob <nicobwan@gmail.com>
2021-07-31 21:56:55 +0300

    jacq_work

---
+++ mukono/logout.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link href="css/layout.css" rel="stylesheet" type="text/css"/>
+<link href="css/menu.css" rel="stylesheet" type="text/css"/>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<div id="Holder">
+<div id="Header"></div>
+<div id="NavBar">
+
+    <nav>
+	<ul>
+	    <li><a href="#">Sign In</a></li>
+		<li><a href="#">Sign Up</a></li>
+		<li><a href="#">Forgot Password</a></li>
+	</ul>
+	</nav>
+</div>
+<div id="Content">
+<div id="PageHeading"></div>
+<div id="ContentLeft">
+  <h1>OWN CATTLE REMOTELY.</h1>
+  <p>&nbsp;</p>
+  <h2>Please Register.</h2>
+  <p>&nbsp;</p>
+  <h3>WITH OFF-DUTY SYSTEM.</h3>
+  <p>&nbsp;</p>
+</div>
+<div id="ContentRight">
+  <p>&nbsp;</p>
+  <p>&nbsp;</p>
+</div>
+</div>
+<div id="Footer"></div>
+</div>
+</body>
+</html>
---
+++ mukono/templates.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link href="css/layout.css" rel="stylesheet" type="text/css"/>
+<link href="css/menu.css" rel="stylesheet" type="text/css"/>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<div id="Holder">
+<div id="Header"></div>
+<div id="NavBar">
+
+    <nav>
+	<ul>
+	    <li><a href="#">Sign In</a></li>
+		<li><a href="#">Sign Up</a></li>
+		<li><a href="#">Forgot Password</a></li>
+	</ul>
+	</nav>
+</div>
+<div id="Content">
+<div id="PageHeading"></div>
+<div id="ContentLeft">
+  <h1 align="left">OWN CATTLE REMOTELY.</h1>
+  <p align="left">&nbsp;</p>
+  <h2 align="left">Please Register.</h2>
+  <p align="left">&nbsp;</p>
+  <h3 align="left">WITH OFF-DUTY SYSTEM.</h3>
+  <p>&nbsp;</p>
+</div>
+<div id="ContentRight">
+  <form id="REGISTER FORM" name="REGISTER FORM" method="post" action="">
+    <table width="400" border="0" align="center">
+      <tr>
+        <td><table border="0">
+          <tr>
+            <td><label>fname
+                <input type="text" name="textfield" />
+            </label></td>
+            <td><label>Lname
+                <input type="text" name="textfield2" />
+            </label></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td><label>Email
+            <input type="text" name="textfield3" />
+        </label></td>
+      </tr>
+      <tr>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td><label>Username
+            <input type="text" name="textfield4" />
+        </label></td>
+      </tr>
+      <tr>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td><table border="0">
+          <tr>
+            <td><label>Password
+                <input type="text" name="textfield5" />
+            </label></td>
+            <td><label>ConfirmPassword
+                <input type="text" name="textfield6" />
+            </label></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td><label>Register
+            <input name="save" type="submit" id="save" />
+        </label></td>
+      </tr>
+      <tr>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td>&nbsp;</td>
+      </tr>
+    </table>
+      </form>
+  </div>
+</div>
+<div id="Footer"></div>
+</div>
+</body>
+</html>
